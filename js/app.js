38fdeed738b84ce618d3a00366583232e8a9e46b
zishan344 <zishanahmed344@gmail.com>
2022-02-15 08:58:47 +0600

    done shoping cart clone

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-function incressAndDecress(product, price, total, isAdd) {
+function incressAndDecress(product, price, isAdd) {
   const productInput = document.getElementById(product + "-input");
   const productInputTaxt = parseInt(productInput.value);
   if (isAdd == true) {
@@ -6,22 +6,42 @@ function incressAndDecress(product, price, total, isAdd) {
   } else if (productInput.value > 1) {
     productInput.value = productInputTaxt - 1;
   }
-  const priceTotal = document.getElementById(total + "-total");
+  const priceTotal = document.getElementById(product + "-total");
   priceTotal.innerText = productInput.value * price;
+  // update subTotal
+  calculateSubtotal();
 }
 
 //update mobile
 document.getElementById("phone-plus").addEventListener("click", function () {
-  incressAndDecress("phone", 1219, "phone", true);
+  incressAndDecress("phone", 1219, true);
 });
 document.getElementById("phone-minus").addEventListener("click", function () {
-  incressAndDecress("phone", 1219, "phone", false);
+  incressAndDecress("phone", 1219, false);
 });
 
 // update cases
 document.getElementById("case-plus").addEventListener("click", function () {
-  incressAndDecress("case", 59, "case", true);
+  incressAndDecress("case", 59, true);
 });
 document.getElementById("case-minus").addEventListener("click", function () {
-  incressAndDecress("case", 59, "case", false);
+  incressAndDecress("case", 59, false);
 });
+function getINput(product) {
+  const productInput = document.getElementById(product + "-input");
+  const productINputText = parseInt(productInput.value);
+  return productINputText;
+}
+//subtotal
+function calculateSubtotal() {
+  const phoneTotal = getINput("phone") * 1219;
+  const caseTotal = getINput("case") * 59;
+  const subTotal = phoneTotal + caseTotal;
+  const tax = subTotal / 10;
+  const totalPrice = tax + subTotal;
+
+  //update on the html
+  document.getElementById("sub-total").innerText = subTotal;
+  document.getElementById("tax-amount").innerText = tax;
+  document.getElementById("total-price").innerText = totalPrice;
+}
